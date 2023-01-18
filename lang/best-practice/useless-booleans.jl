015a9d8d1bcb40dff81aac978bec3cd3a1b3c5b2
Avik Sengupta <avik@sengupta.net>
2023-01-18 23:24:48 +0000

    useless booleans rule

---
+++ lang/best-practice/useless-booleans.jl
@@ -0,0 +1,53 @@
+
+# ruleid:useless-booleans
+if true
+    println(1)
+else
+    println(2)
+end
+
+# ruleid:useless-booleans
+if false
+    println(1)
+else
+    println(2)
+end
+
+x = false 
+
+# ruleid:useless-booleans
+if x && true
+    println(1)
+else
+    println(2)
+end
+
+# ruleid:useless-booleans
+if x && false
+    println(1)
+else
+    println(2)
+end
+
+# ruleid:useless-booleans
+if x || false
+    println(1)
+else
+    println(2)
+end
+
+# ruleid:useless-booleans
+if x || true
+    println(1)
+else
+    println(2)
+end
+
+while true #works! 
+    break
+end
+
+# ruleid:useless-booleans
+while x && true
+    break
+end
