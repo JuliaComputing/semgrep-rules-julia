c263eb7aad14afbbf6772bad04f1800d2b6a1c1f
Iulia Dumitru <iuliadmtru@gmail.com>
2023-07-06 16:07:41 +0300

    Change rule id to threads-macro-not-recommended

--- threads/threads.jl
+++ threads/threads.jl
@@ -2,17 +2,17 @@ using Base.Threads
 
 a = zeros(10)
 
-# ruleid: threads-macro
+# ruleid: threads-macro-not-recommended
 Threads.@threads for i = 1:10
     a[i] = Threads.threadid()
 end
 
-# ruleid: threads-macro
+# ruleid: threads-macro-not-recommended
 @threads for i = 1:10
     a[i] = threadid()
 end
 
-# ok: threads-macro
+# ok: threads-macro-not-recommended
 Threads.@threads :static for i = 1:10
     a[i] = Threads.threadid()
 end
